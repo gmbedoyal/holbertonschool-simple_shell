6ae13333887f25be3d4d5c0e780481eeec1dfe10
Miguel Bedoya <gmbl07@hotmail.com>
2022-12-15 10:50:25 -0500

    ejercicios shell

---
+++ prompt.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    size_t n = 1;
+    char *buf = NULL;
+
+    printf("$ ");
+    getline(&buf, &n, stdin);
+    printf("%s", buf);
+    free(buf);
+    return (0);
+}
